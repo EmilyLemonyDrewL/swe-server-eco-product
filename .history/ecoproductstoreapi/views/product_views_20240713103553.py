c23866a99f054dd74df2fa3c2d2aa76641e28eb7
Abigail-DePriest <abby.depriest@gmail.com>
2024-07-13 11:39:01 -0500

    created Cart model and made migrations

--- .history/ecoproductstoreapi/views/product_views_20240713103553.py
+++ .history/ecoproductstoreapi/views/product_views_20240713103553.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseServerError
-from rest_framework.viewsets import ViewSet
+from rest_framework.views import ViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import serializers, status
