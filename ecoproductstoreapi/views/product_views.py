40e0d683b11613bc3fec95af7d600cc572df9834
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-07-15 20:02:54 -0500

    read, create, and update for products tested in postman.

--- ecoproductstoreapi/views/product_views.py
+++ ecoproductstoreapi/views/product_views.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import serializers, status
-from ecoproductstoreapi.models import Product
+from ecoproductstoreapi.models import Product, Category
 
 class ProductView(ViewSet):
     
@@ -17,27 +17,42 @@ class ProductView(ViewSet):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def create (self, request):
+    def create(self, request):
+        if 'product_image' not in request.data:
+            raise serializers.ValidationError({'product_image': 'This field is required.'})
+        
+        category_id = request.data['category']
+        category = Category.objects.get(pk=category_id)
         
         product = Product.objects.create(
-            name = request.data['name'],
-            price = request.data['price'],
-            description = request.data['description'],
-            product_image = request.data['image'],
-            category = request.data['category']
+            name=request.data['name'],
+            price=request.data['price'],
+            description=request.data['description'],
+            product_image=request.data['product_image'],
+            category=category
         )
         serializer = ProductSerializer(product)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    def update (self, request, pk=None):
-        product = Product.objects.get(pk=pk)
+
+    def update(self, request, pk=None):
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if 'product_image' not in request.data:
+            raise serializers.ValidationError({'product_image': 'This field is required.'})
+
+        category_id = request.data['category']
+        category = Category.objects.get(pk=category_id)
+
         product.name = request.data['name']
         product.price = request.data['price']
         product.description = request.data['description']
-        product.product_image = request.data['image']
-        product.category = request.data['category']
+        product.product_image = request.data['product_image']
+        product.category = category
         product.save()
-        
+
         serializer = ProductSerializer(product)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
