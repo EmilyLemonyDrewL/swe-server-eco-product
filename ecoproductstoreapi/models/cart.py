c15cad0d1285fb5f185cdfb7ed55f3b421c24f54
Abigail-DePriest <abby.depriest@gmail.com>
2024-07-30 19:16:18 -0500

    added CartProducts model and updated Cart model

--- ecoproductstoreapi/models/cart.py
+++ ecoproductstoreapi/models/cart.py
@@ -3,7 +3,6 @@ from .user import User
 from .product import Product
 
 class Cart(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    quantity= models.IntegerField(default=1)
+    
